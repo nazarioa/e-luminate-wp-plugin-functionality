7f7d020078ac8c35aeeab25771aec4bbd5a0693a
Nazario A. Ayala <nazario@niztech.com>
2018-06-04 00:08:58 -0700

    INIT

---
+++ eluminate-functionality.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nazario
+ * Date: 6/4/18
+ * Time: 12:12 AM
+ *
+ * @link              http://example.com
+ * @since             1.0.0
+ * @package           EluminateFunctionality
+ *
+ * @wordpress-plugin
+ * Plugin Name: E-luminate Youtube Gallery
+ * Description: Leverages youtube-embed-plus code to create a simple way to embed youtube videos
+ * Version: 1.0.0
+ * Author: Niztech
+ * Author URI: http://www.niztech.com
+ * Text Domain: eluminate_functionality
+ * Domain Path: /languages
+ * Requires License:    no
+ */
+
