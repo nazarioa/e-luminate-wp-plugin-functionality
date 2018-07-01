915b8e8965377d97e8f2555e86e9d60fd91930ab
Nazario A. Ayala <nazario@niztech.com>
2018-06-30 22:18:39 -0700

    Changed the menu_icon to be a video

--- eluminate-functionality.php
+++ eluminate-functionality.php
@@ -67,6 +67,7 @@ if ( ! function_exists( 'register_eluminate_post_types' ) ) {
 			'public'              => true,
 			'show_ui'             => true,
 			'show_in_menu'        => true,
+			'menu_icon' => 'dashicons-video-alt',
 			'menu_position'       => 5,
 			'show_in_admin_bar'   => true,
 			'show_in_nav_menus'   => true,
